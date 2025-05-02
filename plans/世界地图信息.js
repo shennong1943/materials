0fd390be4467f74e76faa71873fe26ef31d4fb58
shennong <hwyzwd@126.com>
2025-05-02 17:11:15 +0800

    Update 世界地图信息.js
    地图信息

--- plans/世界地图信息.js
+++ plans/世界地图信息.js
@@ -1,1 +1,566 @@
+const worldJson1 = [
+  {
+    "name": "中国",
+    "name_en": "China",
+    "longitude": 116.4074,
+    "latitude": 39.9042
+  },
+  {
+    "name": "美国",
+    "name_en": "United States",
+    "longitude": -77.0369,
+    "latitude": 38.9072
+  },
+  {
+    "name": "日本",
+    "name_en": "Japan",
+    "longitude": 139.6917,
+    "latitude": 35.6895
+  },
+  {
+    "name": "德国",
+    "name_en": "Germany",
+    "longitude": 13.4050,
+    "latitude": 52.5200
+  },
+  {
+    "name": "英国",
+    "name_en": "United Kingdom",
+    "longitude": -0.1278,
+    "latitude": 51.5074
+  },
+  {
+    "name": "法国",
+    "name_en": "France",
+    "longitude": 2.3522,
+    "latitude": 48.8566
+  },
+  {
+    "name": "印度",
+    "name_en": "India",
+    "longitude": 77.2090,
+    "latitude": 28.6139
+  },
+  {
+    "name": "巴西",
+    "name_en": "Brazil",
+    "longitude": -47.9297,
+    "latitude": -15.7801
+  },
+  {
+    "name": "俄罗斯",
+    "name_en": "Russia",
+    "longitude": 37.6176,
+    "latitude": 55.7558
+  },
+  {
+    "name": "加拿大",
+    "name_en": "Canada",
+    "longitude": -75.6972,
+    "latitude": 45.4215
+  },
+  {
+    "name": "澳大利亚",
+    "name_en": "Australia",
+    "longitude": 149.1289,
+    "latitude": -35.2809
+  },
+  {
+    "name": "韩国",
+    "name_en": "South Korea",
+    "longitude": 126.9780,
+    "latitude": 37.5665
+  },
+  {
+    "name": "意大利",
+    "name_en": "Italy",
+    "longitude": 12.4964,
+    "latitude": 41.9028
+  },
+  {
+    "name": "西班牙",
+    "name_en": "Spain",
+    "longitude": -3.7038,
+    "latitude": 40.4168
+  },
+  {
+    "name": "墨西哥",
+    "name_en": "Mexico",
+    "longitude": -99.1332,
+    "latitude": 19.4326
+  },
+  {
+    "name": "印度尼西亚",
+    "name_en": "Indonesia",
+    "longitude": 106.8456,
+    "latitude": -6.2088
+  },
+  {
+    "name": "荷兰",
+    "name_en": "Netherlands",
+    "longitude": 4.8952,
+    "latitude": 52.3702
+  },
+  {
+    "name": "沙特阿拉伯",
+    "name_en": "Saudi Arabia",
+    "longitude": 46.6753,
+    "latitude": 24.7136
+  },
+  {
+    "name": "土耳其",
+    "name_en": "Turkey",
+    "longitude": 32.8597,
+    "latitude": 39.9334
+  },
+  {
+    "name": "瑞士",
+    "name_en": "Switzerland",
+    "longitude": 8.5417,
+    "latitude": 47.3769
+  },
+  {
+    "name": "阿根廷",
+    "name_en": "Argentina",
+    "longitude": -58.3816,
+    "latitude": -34.6037
+  },
+  {
+    "name": "瑞典",
+    "name_en": "Sweden",
+    "longitude": 18.0686,
+    "latitude": 59.3293
+  },
+  {
+    "name": "挪威",
+    "name_en": "Norway",
+    "longitude": 10.7522,
+    "latitude": 59.9139
+  },
+  {
+    "name": "波兰",
+    "name_en": "Poland",
+    "longitude": 21.0122,
+    "latitude": 52.2297
+  },
+  {
+    "name": "泰国",
+    "name_en": "Thailand",
+    "longitude": 100.5018,
+    "latitude": 13.7563
+  },
+  {
+    "name": "新加坡",
+    "name_en": "Singapore",
+    "longitude": 103.8198,
+    "latitude": 1.3521
+  },
+  {
+    "name": "马来西亚",
+    "name_en": "Malaysia",
+    "longitude": 101.6869,
+    "latitude": 3.1390
+  },
+  {
+    "name": "菲律宾",
+    "name_en": "Philippines",
+    "longitude": 120.9842,
+    "latitude": 14.5995
+  },
+  {
+    "name": "越南",
+    "name_en": "Vietnam",
+    "longitude": 105.8342,
+    "latitude": 21.0278
+  },
+  {
+    "name": "南非",
+    "name_en": "South Africa",
+    "longitude": 28.1871,
+    "latitude": -25.7469
+  },
+  {
+    "name": "埃及",
+    "name_en": "Egypt",
+    "longitude": 31.2357,
+    "latitude": 30.0444
+  },
+  {
+    "name": "新西兰",
+    "name_en": "New Zealand",
+    "longitude": 174.7762,
+    "latitude": -41.2865
+  },
+  {
+    "name": "丹麦",
+    "name_en": "Denmark",
+    "longitude": 12.5683,
+    "latitude": 55.6761
+  },
+  {
+    "name": "芬兰",
+    "name_en": "Finland",
+    "longitude": 24.9384,
+    "latitude": 60.1699
+  },
+  {
+    "name": "比利时",
+    "name_en": "Belgium",
+    "longitude": 4.3517,
+    "latitude": 50.8503
+  },
+  {
+    "name": "奥地利",
+    "name_en": "Austria",
+    "longitude": 16.3738,
+    "latitude": 48.2082
+  },
+  {
+    "name": "爱尔兰",
+    "name_en": "Ireland",
+    "longitude": -6.2603,
+    "latitude": 53.3498
+  },
+  {
+    "name": "葡萄牙",
+    "name_en": "Portugal",
+    "longitude": -9.1393,
+    "latitude": 38.7223
+  },
+  {
+    "name": "希腊",
+    "name_en": "Greece",
+    "longitude": 23.7275,
+    "latitude": 37.9838
+  },
+  {
+    "name": "捷克",
+    "name_en": "Czech Republic",
+    "longitude": 14.4378,
+    "latitude": 50.0755
+  },
+  {
+    "name": "匈牙利",
+    "name_en": "Hungary",
+    "longitude": 19.0402,
+    "latitude": 47.4979
+  },
+  {
+    "name": "乌克兰",
+    "name_en": "Ukraine",
+    "longitude": 30.5234,
+    "latitude": 50.4501
+  },
+  {
+    "name": "罗马尼亚",
+    "name_en": "Romania",
+    "longitude": 26.1025,
+    "latitude": 44.4268
+  },
+  {
+    "name": "智利",
+    "name_en": "Chile",
+    "longitude": -70.6693,
+    "latitude": -33.4489
+  },
+  {
+    "name": "哥伦比亚",
+    "name_en": "Colombia",
+    "longitude": -74.0721,
+    "latitude": 4.7110
+  },
+  {
+    "name": "秘鲁",
+    "name_en": "Peru",
+    "longitude": -77.0428,
+    "latitude": -12.0464
+  },
+  {
+    "name": "委内瑞拉",
+    "name_en": "Venezuela",
+    "longitude": -66.9036,
+    "latitude": 10.4806
+  },
+  {
+    "name": "以色列",
+    "name_en": "Israel",
+    "longitude": 35.2137,
+    "latitude": 31.7683
+  },
+  {
+    "name": "阿联酋",
+    "name_en": "United Arab Emirates",
+    "longitude": 54.3770,
+    "latitude": 24.4539
+  },
+  {
+    "name": "卡塔尔",
+    "name_en": "Qatar",
+    "longitude": 51.5310,
+    "latitude": 25.2769
+  },
+  {
+    "name": "科威特",
+    "name_en": "Kuwait",
+    "longitude": 47.9774,
+    "latitude": 29.3759
+  }
+]
 
+const world2Json = [
+  {
+    "name": "中国",
+    "name_en": "China",
+    "lonlat": "116.407400,39.904200"
+  },
+  {
+    "name": "美国",
+    "name_en": "United States",
+    "lonlat": "-77.036900,38.907200"
+  },
+  {
+    "name": "日本",
+    "name_en": "Japan",
+    "lonlat": "139.691700,35.689500"
+  },
+  {
+    "name": "德国",
+    "name_en": "Germany",
+    "lonlat": "13.405000,52.520000"
+  },
+  {
+    "name": "英国",
+    "name_en": "United Kingdom",
+    "lonlat": "-0.127800,51.507400"
+  },
+  {
+    "name": "法国",
+    "name_en": "France",
+    "lonlat": "2.352200,48.856600"
+  },
+  {
+    "name": "印度",
+    "name_en": "India",
+    "lonlat": "77.209000,28.613900"
+  },
+  {
+    "name": "巴西",
+    "name_en": "Brazil",
+    "lonlat": "-47.929700,-15.780100"
+  },
+  {
+    "name": "俄罗斯",
+    "name_en": "Russia",
+    "lonlat": "37.617600,55.755800"
+  },
+  {
+    "name": "加拿大",
+    "name_en": "Canada",
+    "lonlat": "-75.697200,45.421500"
+  },
+  {
+    "name": "澳大利亚",
+    "name_en": "Australia",
+    "lonlat": "149.128900,-35.280900"
+  },
+  {
+    "name": "韩国",
+    "name_en": "South Korea",
+    "lonlat": "126.978000,37.566500"
+  },
+  {
+    "name": "意大利",
+    "name_en": "Italy",
+    "lonlat": "12.496400,41.902800"
+  },
+  {
+    "name": "西班牙",
+    "name_en": "Spain",
+    "lonlat": "-3.703800,40.416800"
+  },
+  {
+    "name": "墨西哥",
+    "name_en": "Mexico",
+    "lonlat": "-99.133200,19.432600"
+  },
+  {
+    "name": "印度尼西亚",
+    "name_en": "Indonesia",
+    "lonlat": "106.845600,-6.208800"
+  },
+  {
+    "name": "荷兰",
+    "name_en": "Netherlands",
+    "lonlat": "4.895200,52.370200"
+  },
+  {
+    "name": "沙特阿拉伯",
+    "name_en": "Saudi Arabia",
+    "lonlat": "46.675300,24.713600"
+  },
+  {
+    "name": "土耳其",
+    "name_en": "Turkey",
+    "lonlat": "32.859700,39.933400"
+  },
+  {
+    "name": "瑞士",
+    "name_en": "Switzerland",
+    "lonlat": "8.541700,47.376900"
+  },
+  {
+    "name": "阿根廷",
+    "name_en": "Argentina",
+    "lonlat": "-58.381600,-34.603700"
+  },
+  {
+    "name": "瑞典",
+    "name_en": "Sweden",
+    "lonlat": "18.068600,59.329300"
+  },
+  {
+    "name": "挪威",
+    "name_en": "Norway",
+    "lonlat": "10.752200,59.913900"
+  },
+  {
+    "name": "波兰",
+    "name_en": "Poland",
+    "lonlat": "21.012200,52.229700"
+  },
+  {
+    "name": "泰国",
+    "name_en": "Thailand",
+    "lonlat": "100.501800,13.756300"
+  },
+  {
+    "name": "新加坡",
+    "name_en": "Singapore",
+    "lonlat": "103.819800,1.352100"
+  },
+  {
+    "name": "马来西亚",
+    "name_en": "Malaysia",
+    "lonlat": "101.686900,3.139000"
+  },
+  {
+    "name": "菲律宾",
+    "name_en": "Philippines",
+    "lonlat": "120.984200,14.599500"
+  },
+  {
+    "name": "越南",
+    "name_en": "Vietnam",
+    "lonlat": "105.834200,21.027800"
+  },
+  {
+    "name": "南非",
+    "name_en": "South Africa",
+    "lonlat": "28.187100,-25.746900"
+  },
+  {
+    "name": "埃及",
+    "name_en": "Egypt",
+    "lonlat": "31.235700,30.044400"
+  },
+  {
+    "name": "新西兰",
+    "name_en": "New Zealand",
+    "lonlat": "174.776200,-41.286500"
+  },
+  {
+    "name": "丹麦",
+    "name_en": "Denmark",
+    "lonlat": "12.568300,55.676100"
+  },
+  {
+    "name": "芬兰",
+    "name_en": "Finland",
+    "lonlat": "24.938400,60.169900"
+  },
+  {
+    "name": "比利时",
+    "name_en": "Belgium",
+    "lonlat": "4.351700,50.850300"
+  },
+  {
+    "name": "奥地利",
+    "name_en": "Austria",
+    "lonlat": "16.373800,48.208200"
+  },
+  {
+    "name": "爱尔兰",
+    "name_en": "Ireland",
+    "lonlat": "-6.260300,53.349800"
+  },
+  {
+    "name": "葡萄牙",
+    "name_en": "Portugal",
+    "lonlat": "-9.139300,38.722300"
+  },
+  {
+    "name": "希腊",
+    "name_en": "Greece",
+    "lonlat": "23.727500,37.983800"
+  },
+  {
+    "name": "捷克",
+    "name_en": "Czech Republic",
+    "lonlat": "14.437800,50.075500"
+  },
+  {
+    "name": "匈牙利",
+    "name_en": "Hungary",
+    "lonlat": "19.040200,47.497900"
+  },
+  {
+    "name": "乌克兰",
+    "name_en": "Ukraine",
+    "lonlat": "30.523400,50.450100"
+  },
+  {
+    "name": "罗马尼亚",
+    "name_en": "Romania",
+    "lonlat": "26.102500,44.426800"
+  },
+  {
+    "name": "智利",
+    "name_en": "Chile",
+    "lonlat": "-70.669300,-33.448900"
+  },
+  {
+    "name": "哥伦比亚",
+    "name_en": "Colombia",
+    "lonlat": "-74.072100,4.711000"
+  },
+  {
+    "name": "秘鲁",
+    "name_en": "Peru",
+    "lonlat": "-77.042800,-12.046400"
+  },
+  {
+    "name": "委内瑞拉",
+    "name_en": "Venezuela",
+    "lonlat": "-66.903600,10.480600"
+  },
+  {
+    "name": "以色列",
+    "name_en": "Israel",
+    "lonlat": "35.213700,31.768300"
+  },
+  {
+    "name": "阿联酋",
+    "name_en": "United Arab Emirates",
+    "lonlat": "54.377000,24.453900"
+  },
+  {
+    "name": "卡塔尔",
+    "name_en": "Qatar",
+    "lonlat": "51.531000,25.276900"
+  },
+  {
+    "name": "科威特",
+    "name_en": "Kuwait",
+    "lonlat": "47.977400,29.375900"
+  }
+]
